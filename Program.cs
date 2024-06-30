202eb07768a75a6740d6984e1f3a1e78d572957c
Juan Cruz <juancruzaraujo@gmail.com>
2024-06-30 20:10:48 -0300

    logueo de errores

--- Program.cs
+++ Program.cs
@@ -11,67 +11,80 @@ namespace SimpleQRGenerator
     {
         public static void Main(string[] args)
         {
-
-            const string C_TEST = "/test";
-            const string C_QRGENERATOR = "/qrgenerator/{inputString}";
-
-            int httpPort = 80;//Convert.ToInt32(args[0]);
-            //int httpsPort = Convert.ToInt32(args[1]);
-
-            Console.WriteLine("Service SimpleQRGenerator start");
-            Console.WriteLine("http port " + httpPort);
-            //Console.WriteLine("https port " + httpsPort);
-
-
-            var builder = WebApplication.CreateBuilder(args);
-            builder.WebHost
-            .UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True")
-            
-            
-            .ConfigureKestrel((context, serverOptions) =>
+            try
             {
+                const string C_TEST = "/test";
+                const string C_QRGENERATOR = "/qrgenerator/{inputString}";
 
-                serverOptions.Listen(IPAddress.Any, httpPort);
+                int httpPort = 80;//Convert.ToInt32(args[0]);
+                                  //int httpsPort = Convert.ToInt32(args[1]);
+
+                Console.WriteLine("Service SimpleQRGenerator start");
+                Console.WriteLine("http port " + httpPort);
+                //Console.WriteLine("https port " + httpsPort);
+
+
+                var builder = WebApplication.CreateBuilder(args);
+                builder.WebHost
+                .UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True")
+
+
+                .ConfigureKestrel((context, serverOptions) =>
+                {
+
+                    serverOptions.Listen(IPAddress.Any, httpPort);
                 //serverOptions.Listen(IPAddress.Loopback, httpsPort, listenOptions =>
                 //{
-                    //listenOptions.UseHttps();//nada de certificados.... por ahora-
+                //listenOptions.UseHttps();//nada de certificados.... por ahora-
 
                 //````});
             });
 
-            //Configuración de registro para limitar los mensajes de registro
-            //builder.Logging.ClearProviders(); // Limpia todos los proveedores de registro existentes
-            //builder.Logging.AddConsole(); // Agrega el proveedor de registro de consola
-            //builder.Logging.SetMinimumLevel(LogLevel.Warning); // Establece el nivel de registro deseado (en este caso, Warning o superior)
+                //Configuración de registro para limitar los mensajes de registro
+                //builder.Logging.ClearProviders(); // Limpia todos los proveedores de registro existentes
+                //builder.Logging.AddConsole(); // Agrega el proveedor de registro de consola
+                //builder.Logging.SetMinimumLevel(LogLevel.Warning); // Establece el nivel de registro deseado (en este caso, Warning o superior)
 
 
-            var app = builder.Build();
+                var app = builder.Build();
 
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet(C_TEST, context => context.Response.WriteAsync("ready online"));
-                endpoints.MapGet(C_QRGENERATOR, GenerateQRCode);
-            });
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGet(C_TEST, context => context.Response.WriteAsync("ready online"));
+                    endpoints.MapGet(C_QRGENERATOR, GenerateQRCode);
+                });
 
-            app.Run();
+                app.Run();
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
 
         }
 
         private static async Task GenerateQRCode(HttpContext context)
         {
-            var inputString = context.Request.RouteValues["inputString"].ToString();
-
-            using (var qrGenerator = new QRCodeGenerator())
-            using (var qrCodeData = qrGenerator.CreateQrCode(inputString, QRCodeGenerator.ECCLevel.Q))
-            using (var qrCode = new QRCode(qrCodeData))
-            using (var bitmap = qrCode.GetGraphic(20))
-            using (var stream = new MemoryStream())
+            try
             {
-                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                stream.Position = 0;
-                context.Response.ContentType = "image/png";
-                await stream.CopyToAsync(context.Response.Body);
+                var inputString = context.Request.RouteValues["inputString"].ToString();
+
+                using (var qrGenerator = new QRCodeGenerator())
+                using (var qrCodeData = qrGenerator.CreateQrCode(inputString, QRCodeGenerator.ECCLevel.Q))
+                using (var qrCode = new QRCode(qrCodeData))
+                using (var bitmap = qrCode.GetGraphic(20))
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    stream.Position = 0;
+                    context.Response.ContentType = "image/png";
+                    await stream.CopyToAsync(context.Response.Body);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
